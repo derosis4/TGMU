d1a037ffbc594ff0555406446148cb8e465925d6
derosis4 <vano-bushmakin@mail.ru>
2025-11-20 01:47:15 +1000

    354

--- 1.py
+++ 1.py
@@ -1,1 +1,12 @@
-print ("Hello World")
+
+a=118
+o=0.118
+
+a=input("Enter a:")
+print(type(a))
+a=int(a)
+print(type(a))
+print(a, o)
+print(f"a={a} , o={o}, a*o={a*o}")
+b=a*o ** 3
+print(f'{b=}')
